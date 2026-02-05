074a125bbe8dd95fbe9382c343b02ad8a6662710
StacksyConsulting <bryant@stacksyconsulting.com>
2026-02-05 12:42:15 +1100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -587,7 +587,8 @@ const ClosePath = () => {
             )}
           </div>
         ) : (
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
+          <div>
+            <div className="grid grid-cols-1 lg:grid-cols-3 gap-6">
             {/* Left Column - Transcript & Questions */}
             <div className="lg:col-span-2 space-y-6">
               {/* Live Transcript */}
@@ -758,6 +759,7 @@ const ClosePath = () => {
               </div>
             </div>
           )}
+          </div>
         )}
       </div>
     </div>
